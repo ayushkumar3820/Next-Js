a6e2889dee1d67038553032a311baeae21b54ff6
ayushkumar3820 <ayushkumarnbd125@gamil.com>
2024-09-08 10:06:55 +0530

    put api

--- src/app/api/user/[id]/route.js
+++ src/app/api/user/[id]/route.js
@@ -1,15 +1,29 @@
-import { NextResponse } from "next/server"
-import { user } from "@/util/db"
+import { NextResponse } from "next/server";
+import { user } from "@/util/db";
 
 export function GET(request, { params }) {
-    // Filter users by id
-    const userData = user.filter(item => item.id == params.id);
-    
-    // Return response
-    return NextResponse.json(
-        userData.length == 0 
-            ? { result: "No Data Found", success: false } 
-            : { result: userData[0], success: true }, 
-        { status: 200 }
-    );
+  // Filter users by id
+  const userData = user.filter((item) => item.id == params.id);
+
+  // Return response
+  return NextResponse.json(
+    userData.length == 0
+      ? { result: "No Data Found", success: false }
+      : { result: userData[0], success: true },
+    { status: 200 }
+  );
 }
+
+export async function PUT(request, Content) {
+    // Get user data from request body
+    let payload = await request.json();
+    payload.id = Content.params.id;
+    console.log(payload);
+    if (!payload.id || !payload.name || !payload.email || !payload.password) {
+      return NextResponse.json(
+        { result: "Invalid Request", success: false },
+        { status: 400 }
+      );
+    }
+    return NextResponse.json({ result: payload, success: true }, { status: 200 });
+  }
--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -5,3 +5,16 @@ export  function GET(){
     return  NextResponse.json(data,{status:200 })
 
 }
+
+export async function POST(request){
+     const payload= await request.json();
+     console.log(payload.name);
+     if(!payload.name || payload.email || !payload.age)
+     {
+        return NextResponse.json({result:"Invalid data",success:false},{status:400})
+     }
+     return  NextResponse.json({result:"valid data",success:true},{status:200})
+    }
+
+
+   
